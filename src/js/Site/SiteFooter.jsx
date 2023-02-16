34cf3f77bbfb59f12bc35774a67749c66a9fca20
Viktor Persson <viktor.persson@arcsin.se>
2023-02-16 19:51:47 +0100

    Update site footer.

--- src/js/Site/SiteFooter.jsx
+++ src/js/Site/SiteFooter.jsx
@@ -2,15 +2,9 @@ export function SiteFooter() {
   return (
     <div className="container">
       <div className="flex h-10 items-center justify-center text-xs space-x-2">
-        <span>
-          Created by
-          <a className="text-sky-600" href="https://vikpe.org">vikpe</a> a.k.a.
-          "XantoM&quot;
-        </span>
+        <span>Created by <a className="text-sky-600" href="https://vikpe.org">vikpe</a> a.k.a. "XantoM"</span>
         <span className="text-gray-400">|</span>
-        <a className="text-sky-600" href="https://github.com/qw-tools/charset">
-          Source on GitHub &#8599;
-        </a>
+        <a className="text-sky-600" href="https://github.com/qw-tools/charset">Source on GitHub &#8599;</a>
       </div>
     </div>
   );
